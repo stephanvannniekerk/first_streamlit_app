22f7907f2f6d18531de4913f85466b9265861c5d
stephanvannniekerk <66555407+stephanvannniekerk@users.noreply.github.com>
2023-11-29 11:26:32 +1300

    Update streamlit_app.py
    Changed Title.
    Input some emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('Omega 3 & Blueberry Oatmeal🫐🥣 ')
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥬')
+streamlit.text('Hard-Boiled Free-Range Eggs🥚')
