7ef2a75a216e06d3afd5e65331186b62f99a1ef1
stephanvannniekerk <66555407+stephanvannniekerk@users.noreply.github.com>
2023-11-29 11:43:32 +1300

    Update streamlit_app.py
    Add another header. Creating you're own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal🫐🥣 ')
 streamlit.text('Kale, Spinach & Rocket Smoothie 🥬')
 streamlit.text('Hard-Boiled Free-Range Eggs🥚')
 streamlit.text('Avocado Toast🥑')
+
+streamlit.header("Create You're Own Smoothie🍌🍓")
